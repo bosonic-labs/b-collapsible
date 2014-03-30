30cfc63212b2cad73a69c36b1951878bd0169399
Raphaël Rougeron <goldoraf@gmail.com>
2014-03-30 10:04:48 +0200

    Small refacto in order to prepare for the next step

--- dist/b-collapsible.js
+++ dist/b-collapsible.js
@@ -2,13 +2,19 @@
     Bosonic.registerElement('b-collapsible', {
         headerClass: 'b-collapsible-header',
         closedClass: 'b-collapsible-closed',
-        readyCallback: function () {
+        createdCallback: function () {
             this.body = this.querySelector(':not(.' + this.headerClass + ')');
             this.header = this.querySelector('.' + this.headerClass);
             if (this.header) {
-                this.header.addEventListener('click', this.toggle.bind(this), false);
+                this.toggleListener = this.toggle.bind(this);
+                this.header.addEventListener('click', this.toggleListener, false);
             }
             this.update();
+        },
+        detachedCallback: function () {
+            if (this.toggleListener) {
+                this.header.removeEventListener('click', this.toggleListener, false);
+            }
         },
         attributeChanged: function (name, oldValue, newValue) {
             if (name === 'active')
@@ -21,11 +27,13 @@
             this.update();
         },
         update: function () {
-            if (this.hasAttribute('active')) {
-                this.body.classList.remove(this.closedClass);
-            } else {
-                this.body.classList.add(this.closedClass);
-            }
+            this.hasAttribute('active') ? this.show() : this.hide();
+        },
+        show: function () {
+            this.body.classList.remove(this.closedClass);
+        },
+        hide: function () {
+            this.body.classList.add(this.closedClass);
         }
     });
 }());
